3ffcd53b68d77c3c03d4ff900fa78086e6c34125
Joe Zuiches <jzuiches@yahoo.com>
2014-12-01 10:33:11 -0800

    password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,7 @@ class User < ActiveRecord::Base
   include BCrypt
 
 validates :name, presence: true
-validates :password, length: { minimum: 5,
-                                maximum: 20,
-                           too_short: "must have at least %{count} characters",
-                            too_long: "must have at most %{count} characters"}
+validates :password, presence: true
 
   def password
     @password ||= Password.new(password_hash)
