c31bea850ac9b7852354d9070f6a7c582ecbca5c
Joe Zuiches <jzuiches@yahoo.com>
2016-07-13 14:13:37 -0700

    adds scss file and gets reacquainted with app

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def full_title(page_title = '')
+    base_title = "Joe Zuiches"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+
 end
--- test/controllers/personal_pages_controller_test.rb
+++ test/controllers/personal_pages_controller_test.rb
@@ -9,7 +9,7 @@ class PersonalPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
 
